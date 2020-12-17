5f974c3206f115670ad60b0011263bfa37c33406
jonathanezeugo <68455131+jonathanezeugo@users.noreply.github.com>
2020-12-17 09:40:38 -0600

    Add files via upload

---
+++ Python_API_Challenge_Files/api_keys.py
@@ -0,0 +1,6 @@
+# OpenWeatherMap API Key
+weather_api_key = "98efd05784ec09eac725db00376fcdd4"
+#weather_api_key = "8ea9927b1f4b8ac0c415e0983a2a4875"
+
+# Google API Key
+g_key = "AIzaSyCfn65fwLEOZbJziU6xscCW_yk5m3ZDC8M"
